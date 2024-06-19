9e1bf040aa4ba4597d481d8ad9751efd271c8a4d
Jacob London <jelondon12@gmail.com>
2024-06-19 15:24:01 -0700

    Add performance metrics

--- fractal.c
+++ fractal.c
@@ -1,55 +1,71 @@
 #include <stdio.h>
 #include <raylib/raylib.h>
 
-static void plot(
-    long double x0, long double y0, int renderx, int rendery)
-{
-    long double xtemp;
-    long double x = 0;
-    long double y = 0;
-    size_t iteration = 0;
-    const size_t MAX_ITERATION = 100;
+static double runtime;
+static double plottingtime;
+static double conversiontime;
 
-    while (( (x*x + y*y) <= 4.0l) && (iteration < MAX_ITERATION)) {
-        xtemp = x*x - y*y + x0;
-        y = 2.0l*x*y + y0;
+/*
+long double
+Iterations: 194
+Runtime: 47.879351
+Plotting time: 36.361787s (75.944611 %) | 0.187432 s/it
+Conversion time: 3.660784s (7.645852 %) | 0.018870 s/it
 
-        x = xtemp;
+double
+Iterations: 214
+Runtime: 51.299721s
+Plotting time: 38.637908s (75.317970 %) | 0.180551 s/it
+Conversion time: 4.040135s (7.875550 %) | 0.018879 s/it
+*/
 
-        iteration += 1;
-    }
-
-    if (iteration == MAX_ITERATION)
-    {
-        return;
-    }
-
-    const int tmp = (int)((float)iteration / (float)MAX_ITERATION * 255.0);
-    const Color color = (Color){tmp, tmp, tmp, 255};
-
-    //printf("It: %zu, Color: %d,%d,%d\n", iteration, color.r, color.g, color.b);
-    DrawPixel(renderx, rendery, color);
-}
-
-/**
- * @returns the view points transformed into base points
- */
+typedef long double double1;
 
 typedef struct double2
 {
-    long double x;
-    long double y;
+    double1 x;
+    double1 y;
 } double2;
 
 typedef struct double4
 {
-    long double x;
-    long double y;
-    long double width;
-    long double height;
+    double1 x;
+    double1 y;
+    double1 width;
+    double1 height;
 } double4;
 
-typedef long double double1;
+#define MAX_ITERATION 100
+#define MAGNITUDE 4.0l
+static void plot(double1 x0, double1 y0, int renderx, int rendery)
+{
+    const double start = GetTime();
+    double1 x = 0;
+    double1 y = 0;
+    double1 x2, y2;
+    int iteration = 0;
+
+    while ((iteration < MAX_ITERATION) &&
+           ( ( (x2=x*x) + (y2=y*y) ) <= MAGNITUDE)
+          )
+    {
+        y = 2.0l*x*y + y0;
+        x = x2 - y2 + x0;
+
+        iteration += 1;
+    }
+
+    Color color = BLACK;
+    if (iteration < MAX_ITERATION)
+    {
+        const int tmp = (int)( (float)iteration / (float)MAX_ITERATION * 255.0f );
+        color = (Color){tmp, tmp, tmp, 255};
+    }
+
+    DrawPixel(renderx, rendery, color);
+    const double end = GetTime();
+    plottingtime += (end - start);
+}
 
 static double2 _window_to_viewport(
     const double1 x_w, const double1 x_wmax, const double1 x_wmin, const double1 x_vmax, const double1 x_vmin,
@@ -64,9 +80,13 @@ static double2 _window_to_viewport(
 
 static double2 window_to_viewport(const double4 *window, const double4 *viewport, const double1 winx, const double1 winy)
 {
-    return _window_to_viewport(
+    const double start = GetTime();
+    const double2 res = _window_to_viewport(
         winx, window->x+window->width, window->x, viewport->x+viewport->width, viewport->x,
         winy, window->y+window->height, window->y, viewport->y+viewport->height, viewport->y);
+    const double end = GetTime();
+    conversiontime += (end - start);
+    return res;
 }
 
 static void window_to_viewport_zoom(const double4 *window, double4 *viewport, const double1 scale, const double1 winx, const double1 winy)
@@ -87,12 +107,18 @@ static int module(int argc, char **argv)
 {
     double4 screen = {0, 0, 800, 400};
     double4 view = {0, 0, 4, 2};
+    double now;
 
     InitWindow(screen.width, screen.height, "Mandelbrot");
+    const double1 screen_width_half = screen.width / 2.0l;
+    const double1 screen_height_half = screen.height / 2.0l;
 
     int i, j;
+    const double start = GetTime();
+    size_t iterations = 0;
     while (!WindowShouldClose())
     {
+        now = GetTime();
         BeginDrawing();
 
         // Pan
@@ -120,24 +146,24 @@ static int module(int argc, char **argv)
             // Zooming in (scale viewport in)
             if (zooming > 0)
             {
-                window_to_viewport_zoom(&screen, &view, 0.75l, mouse.x-screen.width/2, mouse.y-screen.height/2);
+                window_to_viewport_zoom(&screen, &view, 0.75l, mouse.x-screen_width_half, mouse.y-screen_height_half);
             }
             // Zooming out (scale viewpoert out)
             else if (zooming < 0)
             {
-                window_to_viewport_zoom(&screen, &view, 1.333l, mouse.x-screen.width/2, mouse.y-screen.height/2);
+                window_to_viewport_zoom(&screen, &view, 1.333l, mouse.x-screen_width_half, mouse.y-screen_height_half);
             }
         }
 
         // Don't draw then going to infinity, just remain clear
         ClearBackground(BLACK);
-        for (i = -screen.height/2; i < screen.height/2; i += 1)
+        for (i = -screen_height_half; i < screen_height_half; i += 1)
         {
-            for (j = -screen.width/2; j < screen.width/2; j += 1)
+            for (j = -screen_width_half; j < screen_width_half; j += 1)
             {
                 // i and j are offset screen coords, remove the offset when drawing
                 const double2 coords = window_to_viewport(&screen, &view, j, i);
-                plot(coords.x, coords.y, j+screen.width/2, i+screen.height/2);
+                plot(coords.x, coords.y, j+(int)screen_width_half, i+(int)screen_height_half);
             }
         }
 
@@ -147,8 +173,23 @@ static int module(int argc, char **argv)
         snprintf(buf, sizeof(buf), "%Lf, %Lf", view.x, view.y);
         DrawText(buf, 0, screen.height-16, 16, WHITE);
         EndDrawing();
+
+        iterations++;
     }
+    const double end = GetTime();
+    runtime = (end - start);
     CloseWindow();
+
+    printf("Iterations: %zu\n", iterations);
+    printf("Runtime: %lfs\n", runtime);
+    printf("Plotting time: %lfs (%lf %%) | %lf s/it\n",
+        plottingtime,
+        plottingtime/runtime*100.0,
+        plottingtime/iterations);
+    printf("Conversion time: %lfs (%lf %%) | %lf s/it\n",
+        conversiontime,
+        conversiontime/runtime*100.0,
+        conversiontime/iterations);
 
     return 0;
 }
